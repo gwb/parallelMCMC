5573e9a4b803c339b183513bae8eb9ff4212664c
Guillaume Basse <gw.basse@gmail.com>
2014-06-09 08:55:38 -0400

    adds easy vhd example

---
+++ models/vanderWerken-easy-vhd.R
@@ -0,0 +1,80 @@
+require(mvtnorm)
+require(clusterGeneration)
+
+
+C <- 4
+d <- 5
+
+sig.ls <- vector('list', C)
+u.ls <- vector('list', C)
+
+w.ls <- c(0.1, 0.2, 0.3, 0.4)
+
+sig.prop <- 0.2*diag(d)
+
+for(i in seq(C)){
+  sig.ls[[i]] <- diag(d)
+  u.ls[[i]] <- runif(d, min=-10, max=10)
+}
+
+dtarget <- function(x){
+  res <- 0
+  for(i in seq(C)){
+    res <- res + w.ls[i] * dmvnorm(x, u.ls[[i]], sig.ls[[i]])
+  }
+  return(res)
+}
+
+rtarget <- function(n){
+  res <- NULL
+  for(j in seq(n)){
+    i <- sample(seq(C), 1, prob=w.ls)
+    res <- rbind(res, rmvnorm(1, u.ls[[i]], sig.ls[[i]]))
+  }
+  return(res)
+}
+
+rtarget.clust <- function(n){
+  res <- NULL
+  clust <- NULL
+  for(j in seq(n)){
+    i <- sample(seq(C), 1, prob=w.ls)
+    res <- rbind(res, rmvnorm(1, u.ls[[i]], sig.ls[[i]]))
+    clust <- c(clust, i)
+  }
+  return(list(res, clust))
+}
+
+draw.normal.proposal <- function(x){
+  return(as.vector(rmvnorm(1, x, sig.prop)))
+}
+
+eval.normal.proposal <- function(new.state, old.state){
+  return(dmvnorm(new.state, mean=old.state, sigma=sig.prop))
+}
+
+# make sure the function never returns NA (take care of over/underflows)
+L <- function(new.state, old.state){
+  r.num <- dtarget(new.state) * eval.normal.proposal(old.state, new.state)
+  r.denom <- dtarget(old.state) * eval.normal.proposal(new.state, old.state)
+  a <- min(1, r.num/r.denom)
+  res <- eval.normal.proposal(new.state, old.state) * a
+  if(is.na(res)){
+    return(0)
+  } else {
+    return(res)
+  }
+}
+
+
+L.vec <- function(new.states, old.state){
+    eval.proposals <- NULL
+    for(i in seq(nrow(new.states))){
+        eval.proposals <- c(eval.proposals, eval.normal.proposal(old.state, new.states[i,]))
+    }
+    r.num <- dtarget(new.states) * eval.proposals
+    r.denom <- dtarget(old.state) * eval.normal.proposal(new.states, old.state)
+    a <- pmin(1, r.num/r.denom)
+    res <- eval.normal.proposal(new.states, old.state) * a
+    return(ifelse(is.na(res), 0, res))
+}
---
+++ remote/test_adaptive_easy_vhd.R
@@ -0,0 +1,43 @@
+source("parallel_adaptive-hd.R")
+source("../bridge-sampling.R")
+source("../graphics.R")
+source("../clustering.R")
+
+set.seed(123, "L'Ecuyer")
+args <- commandArgs(trailingOnly=TRUE)
+ncores <- as.numeric(args[1])
+
+source("../models/vanderWerken-easy-vhd.R")
+
+
+
+# # # # # # # # # # # #
+# Running the sampler
+# # # # # # # # # # # #
+
+# We initialize at the actual centers. See how we do in this simple case
+init.points <- do.call('rbind', lapply(1:4, function(i) do.call('rbind', u.ls)))
+
+
+# Number of desired partitions
+K <- 4
+
+# create the samplers
+mh.sampler <- get.mv.mh(2000, draw.normal.proposal, eval.normal.proposal, burnin=500)
+mh.explorer <- get.mv.mh(4000, draw.normal.proposal, eval.normal.proposal, burnin=1000)
+
+# explore the surface
+init.draws <- init.exploration(dtarget, mh.explorer, init.points, ncores=ncores)
+
+# setting up clustering method
+spec.clust <- function(X, C){
+    return(spectral.clustering(X, C, L, nsub=100,L.vec=L.vec))
+}
+
+# runs the adaptive sampling algorithm
+algo.res <- adaptive.sampling(dtarget, mh.sampler, K, init.draws, spec.clust, 5, ncores=K)
+
+
+save(u.ls, sig.ls, init.draws, init.points, algo.res, file="results/vanderwerken-1-5D-p-simple.rdata")
+
+
