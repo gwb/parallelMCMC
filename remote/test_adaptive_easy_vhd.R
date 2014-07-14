43b6c7d2fcca797c98cfdbae38b1b4573d57b7fe
gwb <gw.basse@gmail.com>
2014-07-14 11:23:01 -0400

    saves test_adaptive_easy before major rewrite

--- remote/test_adaptive_easy_vhd.R
+++ remote/test_adaptive_easy_vhd.R
@@ -23,7 +23,7 @@ init.points <- do.call('rbind', lapply(1:4, function(i) do.call('rbind', u.ls)))
 K <- 4
 
 # create the samplers
-mh.sampler <- get.mv.mh(2000, draw.normal.proposal, eval.normal.proposal, burnin=500)
+mh.sampler <- get.mv.mh(1000, draw.normal.proposal, eval.normal.proposal, burnin=200)
 mh.explorer <- get.mv.mh(4000, draw.normal.proposal, eval.normal.proposal, burnin=1000)
 
 # explore the surface
@@ -31,11 +31,12 @@ init.draws <- init.exploration(dtarget, mh.explorer, init.points, ncores=ncores)
 
 # setting up clustering method
 spec.clust <- function(X, C){
-    return(spectral.clustering(X, C, L, nsub=100,L.vec=L.vec))
+    #return(spectral.clustering(X, C, L, nsub=100,L.vec=L.vec))
+    return(spectral.clustering(X, C, L.vec.sym, get.K.hat, .get.closeness.centers, nsub=200))
 }
 
 # runs the adaptive sampling algorithm
-algo.res <- adaptive.sampling(dtarget, mh.sampler, K, init.draws, spec.clust, 5, ncores=K)
+algo.res <- adaptive.sampling(dtarget, mh.sampler, K, init.draws, spec.clust, 7, ncores=K)
 
 
 save(u.ls, sig.ls, init.draws, init.points, algo.res, file="results/vanderwerken-1-5D-p-easy.rdata")
