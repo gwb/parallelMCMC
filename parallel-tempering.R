b6711521734947ff7cd738c5f0e39ab9f66004b8
gwb <gw.basse@gmail.com>
2014-07-29 09:53:21 -0400

    adds parallel tempering

---
+++ parallel-tempering.R
@@ -0,0 +1,48 @@
+
+
+parallel.tempering <- function(targets.ls, single.rproposals.ls, single.dproposals.ls, init.states.mat, niter){
+
+    nchains <- length(targets.ls)
+    
+    X.ls <- vector('list', nchains)
+    L.ls <- vector('list', nchains)
+    R.ls <- vector('list', nchains)
+    
+    X.tm1 <- init.states.mat
+    acceptance.count <- rep(0, nchains)
+    swap.acceptance.count <- 0
+    
+    for(i in seq(niter)){
+        # regular state update
+        for(j in seq(nchains)){
+            L.t <- single.rproposals.ls[[j]](X.tm1[j,])
+            r.num <- targets.ls[[j]](L.t) * single.dproposals.ls[[j]](X.tm1[j,], L.t)
+            r.denom <- targets.ls[[j]](X.tm1[j,]) * single.dproposals.ls[[j]](L.t, X.tm1[j,])
+            r <- r.num/r.denom
+            X.ls[[j]] <- rbind(X.ls[[j]], X.tm1[j,])
+            L.ls[[j]] <- rbind(L.ls[[j]], L.t)
+            R.ls[[j]] <- c(R.ls[[j]], min(r,1))
+            
+            if(runif(1) < r){
+                acceptance.count[j] <- acceptance.count[j] + 1
+                X.tm1[j,] <- L.t
+            }
+        }
+
+        # state swapping update
+        indx <- sample(seq(nchains), replace=F)
+        r.num <- targets.ls[[indx[1]]](X.ls[[indx[2]]][i,]) * targets.ls[[indx[2]]](X.ls[[indx[1]]][i,])
+        r.denom <- targets.ls[[indx[1]]](X.ls[[indx[1]]][i,]) * targets.ls[[indx[2]]](X.ls[[indx[2]]][i,])
+        if(runif(1) < r.num / r.denom){
+            swap.acceptance.count <- swap.acceptance.count + 1
+            tmp <- X.ls[[indx[1]]][i,]
+            X.ls[[indx[1]]][i,] <-  X.ls[[indx[2]]][i,]
+            X.ls[[indx[2]]][i,] <- tmp
+            X.tm1[indx[1],] <- X.ls[[indx[1]]][i,]
+            X.tm1[indx[2],] <- X.ls[[indx[2]]][i,]
+        }
+    }
+    
+    return(list(X.ls, L.ls, R.ls, swap.acceptance.count/niter, acceptance.count/niter))
+}
+
