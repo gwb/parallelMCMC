f3299c62962b456c4bb84e5a17943e61522d2eb0
gwb <gw.basse@gmail.com>
2014-09-24 08:14:44 -0400

    New clustering algorithm

--- clustering.R
+++ clustering.R
@@ -1,4 +1,8 @@
-
+#
+# The algorithm for spectral partitioning has been rewritten to reflect two major changes:
+# 1) the use of the proposal kernel Q rather than the transition kernel L as a distance metric
+# 2) the use of Bengio et al's method for out of sample extension of clustering
+#
 
 cluster.kmeans <- function(X,K,iter.max=50,nstart=20){
   # each row of X should be a point. so the number of columns of X should be the dimension of the space
@@ -7,326 +11,228 @@ cluster.kmeans <- function(X,K,iter.max=50,nstart=20){
 }
 
 
-# # # # # # # # # 
-# Regular Kmeans
-# # # # # # # # #
 
 
-regular.clustering <- function(X, K){
-  kres <- cluster.kmeans(X, K)
-  centers <- kres$centers
-  fn <- function(x){
-    res <- NULL
-    for(i in seq(nrow(centers))){
-      res <- c(res, sum((centers[i,]-x)^2))
+# # # # # # # # # # # # # # # # # # # # #
+# Spectral Partitioning | Slow version  # 
+# # # # # # # # # # # # # # # # # # # # #
+
+# obtaining the transformed kernel
+# (equivalent to first computing the Gram matrix, and then normalizing with t(D^-1/2) M D^-1/2
+
+get.transformed.kernel <- function(X, Q){
+    K <- function(new.state, old.state){
+        av.1 <- sum(Q(new.state, X))/nrow(X)
+        av.2 <- sum(Q(X, old.state))/nrow(X)
+        return(Q(new.state, old.state)/sqrt(av.1*av.2))
     }
-    return(as.vector(which.min(res)))
-  }
-  return(list(indicator=fn, centers=centers))
+    return(K)
 }
 
 
-# # # # # # # # # # # # # # 
-# Kmeans using projections
-# # # # # # # # # # # # # #
-
-
-# Projects the points on a random subspace of dimension d,
-# and returns a function cfn that takes a point in the original
-# scale, and cluster it. 
-# Args:
-# d is dimension of projected space
-# K is number of clusters
-# returns: an indicator function for which cluster
-#          a certain point is in, and the centers (in
-#          in the projected space)
-.proj.clustering <- function(X, d, K){
-  P <- get.subspace.D(ncol(X), d)
-  pX <- t(P %*% t(X))
-  kres = cluster.kmeans(pX, K)
-  centers=kres$centers
-  fn <- function(x){
-    px <- as.vector(P %*% x)
-    res <- NULL
-    for(i in seq(1, nrow(centers))){
-      res <- c(res, sum((centers[i,] - px)^2))
-    }
-    return(as.vector(which.min(res)))
-  }
-  return(list(cfn=fn, centers=centers))
-}
-
-
-
-# Computes the center of the clusters in the original scale
-# Args:
-# X is a matrix of data (each column is a data point)
-# cfn an indicator function for the clusters
-# K the number of clusters
-get.augmented.centers <- function(X, cfn, K){
-  clusters <- vector("list", K)
-
-  for(i in seq(nrow(X))){
-    clust.idx <- cfn(X[i,])
-    clusters[[clust.idx]] <- rbind(clusters[[clust.idx]], X[i,])
-  }
-
-  centers <- NULL
-  for(j in seq(K)){
-    centers <- rbind(centers, colMeans(clusters[[j]]))
-  }
-  
-  return(centers)
-}
-
-
-proj.clustering <- function(X, d, K){
-  cfn <- .proj.clustering(X, d, K)
-  centers <- get.augmented.centers(X, cfn, K)
-  return(list(indicator=cfn, centers=centers))
-}
-
-
-# # # # # # # # # # # # # # 
-# Density based-clustering
-# # # # # # # # # # # # # #
-
-detect.size.drop <- function(ds){
-  n.clust <- max(unique(ds$cluster))
-  size.clust <- NULL
-  for(i in seq(n.clust)){
-    size.clust <- c(size.clust, sum(ds$cluster==i))
-  }
-  sorted.clust <- sort(size.clust, decreasing=T, index.return=T)
-  drops <- NULL
-  for(i in seq(2,n.clust)){
-    drops[i] <- sorted.clust$x[i] / sorted.clust$x[i-1]
-  }
-  print(drops)
-  min.drop <- which.min(drops)
-  high.clust <- sorted.clust$ix[seq(1,min.drop-1)]
-  low.clust <- sorted.clust$ix[seq(min.drop, n.clust)]
-  return(list(high=high.clust, low=low.clust))
-}
-
-
-thin.high.density <- function(draws, ds, high, thin.p=0.3, remove.noise=T){
-  idx.0 <- which(ds$cluster==0)
-
-  exclude.idx <- vector('list', length(high))
-
-  for(j in seq(length(high))){
-    exclude.idx[[j]] <- which(ds$cluster == high[j])
-  }
-
-  if(remove.noise){
-    tmp.draws <- draws[-c(idx.0,do.call('c', exclude.idx)),]
-  } else {
-    tmp.draws <- draws[-do.call('c', exclude.idx),]
-  }
-  
-  n <- round(thin.p*length(exclude.idx[[length(high)]]))
-
-  print(n)
-
-  for(j in seq(length(high))){
-    tmp.draws <- rbind(tmp.draws, draws[sample(exclude.idx[[j]], n),])
-  }
-  return(tmp.draws)
-}
-
-
-smart.thinning <- function(draws, thin.p=0.2, scan.eps=0.3){
-  niter <- ceiling(draws/7000)
-  shuffled.idx <- sample(seq(nrow(draws)), nrow(draws))
-  shuffled.draws <- draws[shuffled.idx,]
-  res.draws <- NULL
-
-  breakpoints <- seq(1, nrow(shuffled.draws), 7000)
-
-  print(paste("total number of iterations:", length(breakpoints)-1))
-  
-  for(i in seq(length(breakpoints)-1)){
-    print(paste(".. iteration:", i, "| length of slice:", breakpoints[i+1] - breakpoints[i]))
-    tmp.draws <- shuffled.draws[seq(breakpoints[i], breakpoints[i+1]),]
-    ds <- dbscan(tmp.draws, scan.eps)
-    high <- detect.size.drop(ds)$high
-    thin.draws <- thin.high.density(tmp.draws, ds, high, thin.p, remove.noise=F)
-    res.draws <- rbind(res.draws, thin.draws)
-  }
-  return(res.draws)
-}
-
-
-thin.high.density.old <- function(draws, ds, thin.p=0.3){
-  idx.0 <- which(ds$cluster==0)
-  idx.1 <- which(ds$cluster==1)
-  idx.2 <- which(ds$cluster==2)
-  idx.3 <- which(ds$cluster==3)
-
-  tmp.draws <- draws[-c(idx.0,idx.1,idx.2,idx.3),]
-
-  #n.1 <- round(thin.p*length(idx.1))
-  #n.2 <- round(thin.p*length(idx.2))
-  n.3 <- round(thin.p*length(idx.3))
-
-  #print(n.1)
-  #print(n.2)
-  print(n.3)
-  
-  tmp.draws <- rbind(tmp.draws, draws[sample(idx.1, n.3),])
-  tmp.draws <- rbind(tmp.draws, draws[sample(idx.2, n.3),])
-  tmp.draws <- rbind(tmp.draws, draws[sample(idx.3, n.3),])
-
-  return(tmp.draws)
-}
-
-
-# # # #
-# Spectral clustering stuff
-# # # #
-
-# this one is ~ 120 times faster than get.K.hat.slow
-get.K.hat <- function(X, Lvec, nsub=500){
-  Y.idx <- sample(seq(1, nrow(X)), nsub)
-  Y <- X[Y.idx,]
-  all.rows <- lapply(seq(nsub), function(i) Lvec(Y, Y[i,]))
-  return(list(K.hat=do.call('rbind', all.rows), Y=Y))
-}
-
-get.K.hat.pruned <- function(X, Lvec, nsub=500, pprune=0.02){
-    nprune <- as.integer(pprune * nsub)
-    ntot <- nsub + nprune
-    Y.idx <- sample(seq(1, nrow(X)), ntot)
-    Y <- X[Y.idx,]
-    all.rows <- lapply(seq(ntot), function(i) Lvec(Y, Y[i,]))
-    K.hat <- do.call('rbind', all.rows)
-    bad.idx <- sort(apply(K.hat, 1, median), index.return=T)$ix[seq(nprune)]
-    Y <- Y[-bad.idx,]
-    K.hat <- K.hat[-bad.idx, -bad.idx]
-    return(list(K.hat=K.hat, Y=Y))
-}
-
-get.K.hat.slow <- function(X, L, nsub=500){
-  Y.idx <- sample(seq(1, nrow(X)), nsub)
-  Y <- X[Y.idx,]
-  get.row <- function(i) sapply(seq(nsub), function(j) L(Y[j,], Y[i,]))
-  all.rows <- lapply(seq(nsub), get.row)
-  return(list(K.hat=do.call('rbind', all.rows), Y=Y))
-}
-
-
-.get.closeness.centers <- function(Y, K.hat, clusters, K){
-  centers <- NULL
-  for(i in seq(K)){
-    clust.idx <- which(clusters==i)
-    sub.K.hat <- K.hat[clust.idx, clust.idx]
-    #sub.K.hat <- t(apply(sub.K.hat, 1, function(x) ifelse(x == 0, 1/min(x), 1/x)))
-    #center.idx <- which.min(colSums(sub.K.hat))
-    center.idx <- which.max(colSums(sub.K.hat))
-    centers <- rbind(centers, Y[clust.idx[center.idx],])
-  }
-  return(centers)
-}
-
-.get.closeness.centers.2 <- function(Y, K.hat, clusters, K, n.dist.0=5){
-  centers <- NULL
-  for(i in seq(K)){
-    clust.idx <- which(clusters==i)
-    n.dist <- min(length(clust.idx), n.dist.0)
-    sub.K.hat <- K.hat[clust.idx, clust.idx]
-    sub.K.hat <- t(apply(sub.K.hat, 1, function(x) ifelse(x == 0, min(x), x)))
-    center.idx <- which.max(apply(sub.K.hat, 2, function(x) sum(sort(x)[1:n.dist])))
-    centers <- rbind(centers, Y[clust.idx[center.idx],])
-  }
-  return(centers)
-}
-
-.get.closeness.centers.3.closure <- function(target.fn){
-    .get.closeness.centers.3 <- function(Y, K.hat, clusters, K){
-        centers <- NULL
-        for(i in seq(K)){
-            clust.idx <- which(clusters==i)
-            max.dens.idx <- which.max(apply(Y[clust.idx,], 1, target.fn))
-            centers <- rbind(centers, Y[clust.idx[max.dens.idx],])
+get.gram.matrix <- function(X, transformed.kernel){
+    N <- nrow(X)
+    M <- matrix(0, nrow=N, ncol=N)
+    for(i in seq(2, N)){
+        for(j in seq(1, i-1)){
+            M[i,j] <- transformed.kernel(X[i,], X[j,])
+            M[j,i] <- M[i,j]
         }
-        return(centers)
     }
-    return(.get.closeness.centers.3)
+    for(i in seq(N)){
+        M[i,i] <- transformed.kernel(X[i,], X[i,])
+    }
+    return(M)
 }
 
-.do.spec.clustering <- function(K.hat, K){
-  D.sq.inv.vec <- 1/sqrt(rowSums(K.hat))
-  if(any(is.na(D.sq.inv.vec))){
-    D.sq.inv.vec[is.na(D.sq.inv.vec)] <- 0
-    warning("Some entries where set to 0 in D^(-1/2)")
-  }
-  D.sq.inv <- diag(D.sq.inv.vec)
-  K.L <- D.sq.inv %*% K.hat %*% D.sq.inv
-  
-  if(any(is.na(K.L)) || any(is.infinite(K.L))){
-    browser()
-  }
-  
-  eigen.res <- eigen(K.L)
-  eigenvecs <- eigen.res$vectors[,seq(1,K)]
-  eigenvals <- eigen.res$values[seq(1,K)]
-  
-  
-  if(is.complex(eigenvecs)){
-    eigenvecs <- matrix(as.double(eigenvecs), nrow=nrow(eigenvecs))
-    warning("complex eigenvectors were converted to real (imaginary part discarded)")
-  }
-  
-  n.eigenvecs <- t(apply(eigenvecs, 1, function(x) x/sqrt(sum(x^2))))
-  #save(n.eigenvecs, K.hat, K, K.L, file="debugfile.rdata")
-  kmeans.res <- kmeans(n.eigenvecs, K, iter.max=400, nstart=15)
-  return(list(cluster=kmeans.res$cluster, centers=kmeans.res$centers, n.eigenvecs=n.eigenvecs, eigenvals=eigenvals))
+get.fk <- function(X, V, lambdas, transformed.kernel){
+    n <- nrow(X)
+    fk <- function(x, k){
+        return( (sqrt(n) / lambdas[k]) * sum(sapply(seq(n), function(i) V[i,k] * transformed.kernel(x, X[i,]))))
+    }
+    return(fk)
 }
 
-.get.cluster.fn <- function(centers, L, Y, eigenvecs){
-  
-  fn <- function(x){
-    x.dist <- L(Y,x)    
-    if(any(is.na(x.dist))){
-      x.dist[is.na(x.dist)] <- 0
-      warning("some NA entries where set to 0")
+get.embedding.fn <- function(X, V, lambdas, transformed.kernel){
+    fk <- get.fk(X, V, lambdas, transformed.kernel)
+    embedding.fn <- function(x){
+        num.dim <- dim(V)[2]
+        n <- nrow(X)
+        return( (1/sqrt(n)) * sapply(seq(num.dim), function(i) fk(x, i)))
     }
-    norm.x.dist <- x.dist / sum(x.dist)
-    px <- as.vector(norm.x.dist %*% eigenvecs)
-    npx <- px / sqrt(sum(px^2))
-    dist.centers <- sapply(seq(nrow(centers)), function(i) sum( (npx-centers[i,])^2 ))
+    return(embedding.fn)
+}
+
+
+get.cluster.fn <- function(centers, embedding.fn){
+    cluster.fn <- function(x){
+        embedded.x <- embedding.fn(x)
+        dist.centers <- sapply(seq(nrow(centers)), function(i) sum( (embedded.x - centers[i,])^2 ))
+        return(which.min(dist.centers))
+    }
+    return(cluster.fn)
+}
+
+get.centers <- function(Y, clusters, nclust){
+    res <- NULL
+    for(i in seq(nclust)){
+        idx.ls <- which(clusters == i)
+        idx <- sample(idx.ls, 1)
+        res <- rbind(res, Y[idx,])
+    }
+    return(res)
+}
+
+.do.spectral.clustering <- function(K.hat, nclust){
+
+    eigen.res <- eigen(K.hat)
+    eigenvecs <- eigen.res$vectors[,seq(1,nclust)]
+    eigenvals <- eigen.res$values[seq(1,nclust)]
+
+    if(is.complex(eigenvecs)){
+        eigenvecs <- matrix(as.double(eigenvecs), nrow=nrow(eigenvecs))
+        warning("complex eigenvectors were converted to real (imaginary part discarded)")
+    }
+
+    n.eigenvecs <- t(apply(eigenvecs, 1, function(x) x/sqrt(sum(x^2))))
+    kmeans.res <- kmeans(n.eigenvecs, nclust, iter.max=400, nstart=15)
+    return(list(cluster=kmeans.res$cluster, centers=kmeans.res$centers, n.eigenvecs=n.eigenvecs, eigenvecs=eigenvecs, eigenvals=eigenvals))
+}
+
+
+spectral.clustering <- function(X, nclust, Q, nsub=100){
+
+    # subsample X
+    Y.idx <- sample(seq(1, nrow(X)), nsub)
+    Y <- X[Y.idx,]
+
+    # transform the kernel and obtain the gram matrix
+    transformed.kernel <- get.transformed.kernel(Y, Q)
+    K.hat <- get.gram.matrix(Y, transformed.kernel)
     
-    return(which.min(dist.centers))
-  }
-  return(fn)
+    # the actual spectral clustering
+    clust.res <- .do.spectral.clustering(K.hat, nclust)
+
+    # creating the cluster indicator function
+    embedding.fn <- get.embedding.fn(Y, clust.res$eigenvecs, clust.res$eigenvals, transformed.kernel)
+    cluster.indicator.fn <- get.cluster.fn(clust.res$centers, embedding.fn)
+
+    # compute the centers used to initialize next mcmc iteration
+    centers <- get.centers(Y, clust.res$cluster, nclust)
+
+    return(list(indicator=cluster.indicator.fn, centers = centers, Y=Y, cluster = clust.res$cluster, embedding.fn=embedding.fn))
 }
 
 
-# note it is the responsibility of the user to check that L, get.K.hat, and cluster.method  work well together
-# both should be vectorized or not.
 
-spectral.clustering <- function(X, K, L, get.K.hat,
-                                center.method=.get.closeness.centers.2,
-                                cluster.method=.get.cluster.fn, nsub=100){
+# # # # # # # # # # # # # # # # # # # # #
+# Spectral Partitioning | Fast version  # 
+# # # # # # # # # # # # # # # # # # # # #
 
-  # getting K.hat and Y
-  K.res <- get.K.hat(X, L, nsub)
-  K.hat <- K.res$K.hat
-  Y <- K.res$Y
-  #save(Y, file="debugfile-1.rdata")
-  
-  # the actual spectral clustering
-  clust.res <- .do.spec.clustering(K.hat, K)
-  
-  # creating the cluster indicator function
-  fn <- cluster.method(clust.res$centers, L, Y, clust.res$n.eigenvecs)
+# Note that the following has been optimized to work for symmetric kernels
 
-  # compute the centers used to initialize next mcmc iteration
-  centers <- center.method(Y, K.hat, clust.res$cluster, K)
-  
-  return(list(indicator=fn, centers=centers, eigenvalues=clust.res$eigenvals, n.eigenvecs=clust.res$n.eigenvecs, Y=Y, clust.clust=clust.res$cluster))
+get.vec.transformed.kernel <- function(X, Q){
+    n <- nrow(X)
+    av2 <- NULL
+    for(i in seq(n)){
+        av2 <- c(av2, sum(Q(X,X[i,])) / n)
+    }
+    
+    K <- function(new.state, old.state=NULL){
+        av.1 <- sum(Q(new.state, X)) / n
+        if(!is.null(old.state)){
+            av2.i <- sum(Q(X,old.state)) / n
+            return(Q(old.state, new.state)/sqrt(av.1*av2.i))
+        }
+        return(Q(X, new.state) / sqrt(av.1*av2))
+    }
+    return(K)
 }
+
+get.vec.transformed.kernel.old <- function(X, Q){
+    K <- function(new.state, old.state){
+        av.1 <- sum(Q(new.state,X)) / nrow(X)
+        if(!is.null(dim(old.state))){
+            av.2 <- sapply(seq(nrow(old.state)), function(i) sum(Q(X, old.state[i,]))/nrow(X))
+        } else {
+            av.2 <- sum(Q(X, old.state))/nrow(X)
+        }
+        return(Q(old.state, new.state)/sqrt(av.1*av.2))
+    }
+    return(K)
+}
+
+get.fast.embedding.fn <- function(X, V, lambdas, transformed.kernel.vec){
+    num.dim <- dim(V)[2]
+    n <- nrow(X)
+    embedding.fn <- function(x){
+        KxXi <- transformed.kernel.vec(x)
+        return( sapply(seq(num.dim), function(k) (1/lambdas[k]) * t(V[,k]) %*% KxXi) )
+    }
+    return(embedding.fn)
+}
+
+
+get.fast.embedding.fn.old <- function(X, V, lambdas, transformed.kernel.vec){
+    num.dim <- dim(V)[2]
+    n <- nrow(X)
+    embedding.fn <- function(x){
+        KxXi <- transformed.kernel.vec(x, X)
+        return( sapply(seq(num.dim), function(k) (1/lambdas[k]) * t(V[,k]) %*% KxXi) )
+    }
+    return(embedding.fn)
+}
+
+spectral.clustering.vec <- function(X, nclust, Q, nsub=100){
+
+    # subsample X
+    Y.idx <- sample(seq(1, nrow(X)), nsub)
+    Y <- X[Y.idx,]
+
+    # transform the kernel and obtain the gram matrix
+    transformed.kernel.vec <- get.vec.transformed.kernel(Y, Q)
+    K.hat <- get.gram.matrix(Y, transformed.kernel.vec)
+    
+    # the actual spectral clustering
+    clust.res <- .do.spectral.clustering(K.hat, nclust)
+
+    # creating the cluster indicator function
+    embedding.fn <- get.fast.embedding.fn(Y, clust.res$eigenvecs, clust.res$eigenvals, transformed.kernel.vec)
+    cluster.indicator.fn <- get.cluster.fn(clust.res$centers, embedding.fn)
+
+    # compute the centers used to initialize next mcmc iteration
+    centers <- get.centers(Y, clust.res$cluster, nclust)
+
+    return(list(indicator=cluster.indicator.fn, centers = centers, Y=Y, cluster = clust.res$cluster, embedding.fn=embedding.fn))
+}
+
+spectral.clustering.vec.old <- function(X, nclust, Q, nsub=100){
+
+    # subsample X
+    Y.idx <- sample(seq(1, nrow(X)), nsub)
+    Y <- X[Y.idx,]
+
+    # transform the kernel and obtain the gram matrix
+    transformed.kernel.vec <- get.vec.transformed.kernel.old(Y, Q)
+    K.hat <- get.gram.matrix(Y, transformed.kernel.vec)
+    
+    # the actual spectral clustering
+    clust.res <- .do.spectral.clustering(K.hat, nclust)
+
+    # creating the cluster indicator function
+    embedding.fn <- get.fast.embedding.fn.old(Y, clust.res$eigenvecs, clust.res$eigenvals, transformed.kernel.vec)
+    cluster.indicator.fn <- get.cluster.fn(clust.res$centers, embedding.fn)
+
+    # compute the centers used to initialize next mcmc iteration
+    centers <- get.centers(Y, clust.res$cluster, nclust)
+
+    return(list(indicator=cluster.indicator.fn, centers = centers, Y=Y, cluster = clust.res$cluster, embedding.fn=embedding.fn))
+}
+
+
+# # # # # #
+# Voronoi #
+# # # # # # 
 
 voronoi.clustering <- function(X, K){
   kres <- cluster.kmeans(X, K)
--- remote/vanderwerken2d/compare_methods.R
+++ remote/vanderwerken2d/compare_methods.R
@@ -12,6 +12,9 @@ bad.res <- res
 load("results/hmc.rdata")
 hmc.res <- res
 
+load("results/hmc_parallel.rdata")
+hmc.par.res <- res
+
 load("results/exploration.rdata")
 
 true.mean <- ws[1] * u.ls[[1]] + ws[2] * u.ls[[2]] + ws[3] * u.ls[[3]] + ws[4] * u.ls[[4]]
@@ -25,19 +28,21 @@ bad.dist <- apply(bad.res, 1, function(x) sum( (x-true.mean)^2 ))
 colMeans(hmc.res)
 hmc.dist <- apply(hmc.res, 1, function(x) sum( (x-true.mean)^2 ))
 
+colMeans(hmc.par.res)
+hmc.par.dist <- apply(hmc.par.res, 1, function(x) sum( (x-true.mean)^2 ))
 
-sds <- c(sd(good.dist), sd(bad.dist), sd(hmc.dist))
-ms <- c(mean(good.dist), mean(bad.dist), mean(hmc.dist))
+
+sds <- c(sd(good.dist), sd(bad.dist), sd(hmc.dist), sd(hmc.par.dist))
+ms <- c(mean(good.dist), mean(bad.dist), mean(hmc.dist), mean(hmc.par.dist))
 emax <- ms + sds
 emin <- ms - sds
-ys <- factor(c("ours", "parallel tempering", "stan(hmc)"))
+ys <- factor(c("ours", "parallel tempering", "stan (hmc)", "stan parallel (hmc)"))
 
 dt <- data.frame(ms=ms,
                  emax=emax,
                  emin=emin,
                  ys=ys)
 
-ggplot(data=dt, aes(ms, ys)) + geom_point() + geom_errorbarh(aes(xmax=emax, xmin=emin, height=0.1))
+g <- ggplot(data=dt, aes(ms, ys)) + geom_point() + geom_errorbarh(aes(xmax=emax, xmin=emin, height=0.1)) + ylab(label="") + xlab(label="Euclidian distance to mean") + theme_bw()
 
-
-
+ggsave(g, file="results/methods-comparison.pdf")
